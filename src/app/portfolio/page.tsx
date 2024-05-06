f0e38985a6c41c963768428153b042399993cbc4
Fuad Talukder <webfuadtk@gmail.com>
2024-05-06 18:14:37 +0600

    retouch some code

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,19 +1,27 @@
 import Image from "next/image";
 import foodsphere from "../../assets/project/foodsphere.jpg";
 import GrocerEase from "../../assets/project/GrocerEase.jpg";
+import { FaDownload } from "react-icons/fa6";
 const PortfolioPage = () => {
   return (
     <div className="p-10">
-      <h1 className="text-white text-3xl mt-10 border-b-4 border-red-500 w-[330px]">
-        Portfolio
-      </h1>
+      <div className="flex justify-between md:pe-28">
+        <h1 className="text-white text-3xl mt-10 border-b-4 border-red-500 md:w-[330px]">
+          Portfolio
+        </h1>
+        <a href="/fuad.pdf" download="fuad.pdf">
+          <button className="text-white mt-10 flex items-center gap-3 border-b border-red-500 hover:border-white hover:text-red-500 transition-all px-6 py-2 rounded-lg">
+            Resume <FaDownload />
+          </button>
+        </a>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10 md:px-28 mt-16">
         {/* portfolio one */}
         <div className="bg-[#161212]">
           <Image src={foodsphere} alt="foodsphere" width={400}></Image>
           <div className="text-[#cacaca] mt-8 px-4 pb-6">
             <div className="flex justify-between items-center">
-              <h5>FoodShpere</h5>
+              <h5 className="text-xl text-white">FoodShpere</h5>
               <button className="text-white flex items-center gap-3 border border-red-500 hover:border-white hover:text-red-500 transition-all px-6 py-2 rounded-lg">
                 MERN Stack
               </button>
@@ -34,8 +42,8 @@ const PortfolioPage = () => {
           <Image src={GrocerEase} alt="foodsphere" width={400}></Image>
           <div className="text-[#cacaca] mt-8 px-4 pb-6">
             <div className="flex justify-between items-center">
-              <h5>GrocerEase</h5>
-              <button className="text-white flex items-center gap-3 border border-red-500 hover:border-white hover:text-red-500 transition-all px-6 py-2 rounded-lg">
+              <h5 className="text-xl text-white">GrocerEase</h5>
+              <button className="text-white flex items-center  gap-3 border border-red-500 hover:border-white hover:text-red-500 transition-all px-6 py-2 rounded-lg">
                 Next js
               </button>
             </div>
@@ -55,7 +63,7 @@ const PortfolioPage = () => {
           <Image src={foodsphere} alt="foodsphere" width={400}></Image>
           <div className="text-[#cacaca] mt-8 px-4 pb-6">
             <div className="flex justify-between items-center">
-              <h5>FoodShpere</h5>
+              <h5 className="text-xl text-white">FoodShpere</h5>
               <button className="text-white flex items-center gap-3 border border-red-500 hover:border-white hover:text-red-500 transition-all px-6 py-2 rounded-lg">
                 MERN Stack
               </button>
