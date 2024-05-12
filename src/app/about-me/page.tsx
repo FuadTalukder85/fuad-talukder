9778f3e4787a3da87bde75a95782650928daac29
Fuad Talukder <webfuadtk@gmail.com>
2024-05-12 18:20:48 +0600

    add animation on home page

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -3,7 +3,8 @@ import Service from "@/component/service/Service";
 import Skill from "@/component/skill/Skill";
 import Link from "next/link";
 import { FaDownload } from "react-icons/fa6";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import "../page.css";
 const AboutMePage = () => {
   const divIntro = {
     hidden: { opacity: 0 },
@@ -18,95 +19,109 @@ const AboutMePage = () => {
     hidden: { opacity: 0, x: 0 },
     visible: { opacity: 1, x: 0 },
   };
-  // const rightContect = {
-  //   hidden: { opacity: 0, x: 500 },
-  //   visible: { opacity: 1, x: 0 },
-  // };
+
   return (
-    <motion.div
-      varients={divIntro}
-      initial="hidden"
-      animate="visible"
-      className="md:flex gap-20 p-10"
-    >
-      <motion.div variants={leftContect} className="md:w-[50%]">
-        <h1 className="text-white text-3xl border-b-4 border-red-500 md:w-[330px]">
-          About Me
-        </h1>
-        <p className="mt-10 text-[#cacaca]">
-          <span className="text-white">Hello, This is Fuad,</span> Complete
-          Diploma in Computer. I am specialized in web design, Frontend
-          technology and dynamic sector, It,s my passion. I am good at MERN
-          Stack, Javascript, Typescript, Next js, Bootstrap, Tailwind css,
-          Daisyui, Firebase, React Hook Form, Zod Validation, Framer Motion,
-          React Components.
-        </p>
-        <p className="text-[#cacaca] mt-5">
-          Design a website can be a delicate and complex process. I will ensure
-          that the correct components are integrated, the website is
-          aesthetically pleasing, and it has the proper elements to generate
-          conversions. Design Modern, Eye-Catching Responsive website is my
-          working Goal. My objective is to provide best quality full projects to
-          my clients and establish a good relationship. I,m fully dedicated to
-          my Profession and Reputetion. Hardworking and Honesty is my policy.
-        </p>
-        <div className="flex gap-2 md:gap-10 mt-16">
-          <div className="bg-[#161212] ps-2 md:ps-10 py-7 rounded-md w-[300px] border border-[#161212] hover:border hover:border-red-500 transition-all">
-            <h5 className="text-[#cacaca]">Email :</h5>
-            <p className="text-white mt-3">webfuadtk@gmail.com</p>
-          </div>
-          <div className="bg-[#161212] ps-2 md:ps-10 py-7 rounded-md w-[300px] border border-[#161212] hover:border hover:border-red-500 transition-all">
-            <h5 className="text-[#cacaca]">Phone :</h5>
-            <p className="text-white mt-3">01756-867585</p>
-          </div>
-        </div>
-        <div>
-          <h1 className="text-white text-3xl mt-16 border-b-4 border-red-500 w-[330px]">
-            Education Qualification
+    <AnimatePresence mode="wait">
+      <motion.div
+        className="side-in"
+        initial={{ scaleY: 0 }}
+        animate={{ scaleY: 0 }}
+        exit={{ scaleY: 0 }}
+        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+      ></motion.div>
+      <motion.div
+        className="side-out"
+        initial={{ scaleY: 1 }}
+        animate={{ scaleY: 0 }}
+        exit={{ scaleY: 0 }}
+        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+      ></motion.div>
+      <motion.div
+        varients={divIntro}
+        initial="hidden"
+        animate="visible"
+        className="md:flex gap-20 p-10"
+      >
+        <motion.div variants={leftContect} className="md:w-[50%]">
+          <h1 className="text-white text-3xl border-b-4 border-red-500 md:w-[330px]">
+            About Me
           </h1>
-        </div>
-        <div className="flex gap-48 mt-10 text-[#cacaca]">
-          <div>
-            <p>2015</p>
+          <p className="mt-10 text-[#cacaca]">
+            <span className="text-white">Hello, This is Fuad,</span> Complete
+            Diploma in Computer. I am specialized in web design, Frontend
+            technology and dynamic sector, It,s my passion. I am good at MERN
+            Stack, Javascript, Typescript, Next js, Bootstrap, Tailwind css,
+            Daisyui, Firebase, React Hook Form, Zod Validation, Framer Motion,
+            React Components.
+          </p>
+          <p className="text-[#cacaca] mt-5">
+            Design a website can be a delicate and complex process. I will
+            ensure that the correct components are integrated, the website is
+            aesthetically pleasing, and it has the proper elements to generate
+            conversions. Design Modern, Eye-Catching Responsive website is my
+            working Goal. My objective is to provide best quality full projects
+            to my clients and establish a good relationship. I,m fully dedicated
+            to my Profession and Reputetion. Hardworking and Honesty is my
+            policy.
+          </p>
+          <div className="flex gap-2 md:gap-10 mt-16">
+            <div className="bg-[#161212] ps-2 md:ps-10 py-7 rounded-md w-[300px] border border-[#161212] hover:border hover:border-red-500 transition-all">
+              <h5 className="text-[#cacaca]">Email :</h5>
+              <p className="text-white mt-3">webfuadtk@gmail.com</p>
+            </div>
+            <div className="bg-[#161212] ps-2 md:ps-10 py-7 rounded-md w-[300px] border border-[#161212] hover:border hover:border-red-500 transition-all">
+              <h5 className="text-[#cacaca]">Phone :</h5>
+              <p className="text-white mt-3">01756-867585</p>
+            </div>
           </div>
           <div>
-            <p className="text-xl font-semibold text-white">
-              Secondary School Certificate (S.S.C.) : Science
-            </p>
-            <p>Mirer Kapashatiya High School, Faridpur</p>
+            <h1 className="text-white text-3xl mt-16 border-b-4 border-red-500 w-[330px]">
+              Education Qualification
+            </h1>
           </div>
-        </div>
+          <div className="flex gap-48 mt-10 text-[#cacaca]">
+            <div>
+              <p>2015</p>
+            </div>
+            <div>
+              <p className="text-xl font-semibold text-white">
+                Secondary School Certificate (S.S.C.) : Science
+              </p>
+              <p>Mirer Kapashatiya High School, Faridpur</p>
+            </div>
+          </div>
 
-        <div className="flex gap-48 mt-5 text-[#cacaca]">
-          <div>
-            <p>2019</p>
+          <div className="flex gap-48 mt-5 text-[#cacaca]">
+            <div>
+              <p>2019</p>
+            </div>
+            <div>
+              <p className="text-xl font-semibold text-white">
+                Diploma in Engineering : Computer
+              </p>
+              <p>National Polytechnic Institute, Faridpur</p>
+            </div>
           </div>
-          <div>
-            <p className="text-xl font-semibold text-white">
-              Diploma in Engineering : Computer
-            </p>
-            <p>National Polytechnic Institute, Faridpur</p>
-          </div>
-        </div>
-      </motion.div>
+        </motion.div>
 
-      <motion.div variants={leftContect} className="md:w-[50%] mt-10 md:mt-0">
-        <Service></Service>
-        <Skill></Skill>
-        <div className="flex gap-10">
-          <Link href="projects">
-            <button className="text-white mt-10 flex items-center gap-3 border-b border-red-500 hover:border-white hover:text-red-500 transition-all px-6 py-2 rounded-lg">
-              View Projects
-            </button>
-          </Link>
-          <a href="/fuad.pdf" download="fuad.pdf">
-            <button className="text-white mt-10 flex items-center gap-3 border-b border-red-500 hover:border-white hover:text-red-500 transition-all px-6 py-2 rounded-lg">
-              Resume <FaDownload />
-            </button>
-          </a>
-        </div>
+        <motion.div variants={leftContect} className="md:w-[50%] mt-10 md:mt-0">
+          <Service></Service>
+          <Skill></Skill>
+          <div className="flex gap-10">
+            <Link href="projects">
+              <button className="text-white mt-10 flex items-center gap-3 border-b border-red-500 hover:border-white hover:text-red-500 transition-all px-6 py-2 rounded-lg">
+                View Projects
+              </button>
+            </Link>
+            <a href="/fuad.pdf" download="fuad.pdf">
+              <button className="text-white mt-10 flex items-center gap-3 border-b border-red-500 hover:border-white hover:text-red-500 transition-all px-6 py-2 rounded-lg">
+                Resume <FaDownload />
+              </button>
+            </a>
+          </div>
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </AnimatePresence>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 import React from "react";
 import AboutMePage from "./about-me/page";
+import "./page.css";
 
 const HomePage = () => {
   return (
-    <div className="">
+    <div>
       <AboutMePage></AboutMePage>
     </div>
   );
