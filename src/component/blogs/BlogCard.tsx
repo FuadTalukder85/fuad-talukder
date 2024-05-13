a431cbd83de06690173782261e9c8a949dad28dd
FuadTalukder85 <webfuadtk@gmail.com>
2024-05-14 02:47:24 +0600

    add color

--- src/component/blogs/BlogCard.tsx
+++ src/component/blogs/BlogCard.tsx
@@ -4,21 +4,19 @@ import { Inputs } from "./BlogForm";
 const BlogCard = ({ allBlogs }: { allBlogs: Inputs[] }) => {
   return (
     <div className="p-10">
-      <div className="grid md:grid-cols-4 gap-5">
+      <div className="grid md:grid-cols-3 gap-5">
         {allBlogs.map((blog, index) => (
           <div key={index} className="md:border-r-2 border-red-500 p-5">
             <div>
               <h1 className="text-red-500 font-bold">
-                Post by :{" "}
-                <p>
-                  <span className="text-white">{blog.names}</span>
-                </p>
+                Post by : <span className="text-white">{blog.names}</span>
               </h1>
               <h1 className="text-red-500">{blog.title}</h1>
-              <div
+              {/* <div
                 dangerouslySetInnerHTML={{ __html: blog.content }}
                 className="text-white mt-3"
-              />
+              /> */}
+              <div className="text-white">{blog.description}</div>
             </div>
           </div>
         ))}
