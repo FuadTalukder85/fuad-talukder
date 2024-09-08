9989116f6d2fe4b0ee25d9cdce8a8169b1009f23
FuadTalukder85 <webfuadtk@gmail.com>
2024-09-09 02:25:50 +0600

    add projects

--- src/component/projects/Projects.tsx
+++ src/component/projects/Projects.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { MdArrowOutward } from "react-icons/md";
 
 const Projects = () => {
@@ -24,24 +25,44 @@ const Projects = () => {
             </div>
           </div>
           <div className="border-b md:border-none py-12 md:py-0 pb-5 md:pb-0">
-            <p className="hidden md:block text-3xl">E-commerce</p>
-            <p className="hidden md:block mt-2">Mern stack</p>
+            <p className="hidden md:block text-3xl">Vogal : E-commerce</p>
+            <p className="hidden md:block mt-2">
+              JS, Next.js, Redux, Framer motion, Node, Mongodb
+            </p>
             <p className="hidden md:block mt-5">Source code : Github</p>
-            <div className="flex gap-5 items-center">
-              <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
-                Frontend code
-                <MdArrowOutward />
-              </button>
-              <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
-                Backend code
-                <MdArrowOutward />
-              </button>
+            <div className="flex gap-5 items-center mt-3">
+              <Link
+                href="https://github.com/FuadTalukder85/data-solution-360"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
+                  Frontend code
+                  <MdArrowOutward />
+                </button>
+              </Link>
+              <Link
+                href="https://github.com/FuadTalukder85/data-solution-360-server"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
+                  Backend code
+                  <MdArrowOutward />
+                </button>
+              </Link>
               <div className="block md:hidden text-2xl">
                 <MdArrowOutward />
               </div>
             </div>
             <div className="text-4xl md:mt-5 hidden md:flex justify-end">
-              <MdArrowOutward />
+              <Link
+                href="https://data-solution-360-rosy.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <MdArrowOutward />
+              </Link>
             </div>
           </div>
         </div>
@@ -50,7 +71,7 @@ const Projects = () => {
         <div className="md:flex items-center gap-7 mt-16 md:mt-0">
           <div className="flex items-center gap-2 md:gap-5 shadow-black shadow-2xl">
             <Image
-              src="https://i.postimg.cc/9MgjTS2B/vogal.png"
+              src="https://i.postimg.cc/DwKRxxsN/Food-Sphere.png"
               alt="vogal"
               width={250}
               height={250}
@@ -61,24 +82,50 @@ const Projects = () => {
             </div>
           </div>
           <div className="border-b md:border-none py-12 md:py-0 pb-5 md:pb-0">
-            <p className="hidden md:block text-3xl">E-commerce</p>
-            <p className="hidden md:block mt-2">Mern stack</p>
+            <p className="hidden md:block text-3xl">FoodSphere</p>
+            <p className="hidden md:block mt-2">
+              TS, React, Redux, Node, Mongodb
+            </p>
             <p className="hidden md:block mt-5">Source code : Github</p>
-            <div className="flex gap-5 items-center">
-              <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
-                Frontend code
-                <MdArrowOutward />
-              </button>
-              <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
-                Backend code
-                <MdArrowOutward />
-              </button>
+            <div className="flex gap-5 items-center mt-3">
+              <Link
+                href="https://github.com/FuadTalukder85/foodSphere-frontend"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
+                  Frontend code
+                  <MdArrowOutward />
+                </button>
+              </Link>
+              <Link
+                href="https://github.com/FuadTalukder85/foodSphere-backend.git"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
+                  Backend code
+                  <MdArrowOutward />
+                </button>
+              </Link>
               <div className="block md:hidden text-2xl">
-                <MdArrowOutward />
+                <Link
+                  href="https://github.com/FuadTalukder85/foodSphere-backend.git"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <MdArrowOutward />
+                </Link>
               </div>
             </div>
             <div className="text-4xl md:mt-5 hidden md:flex justify-end">
-              <MdArrowOutward />
+              <Link
+                href="https://food-sphere-frontend.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <MdArrowOutward />
+              </Link>
             </div>
           </div>
         </div>
@@ -87,7 +134,7 @@ const Projects = () => {
         <div className="md:flex items-center gap-7 mt-16 md:mt-0">
           <div className="flex items-center gap-2 md:gap-5 shadow-black shadow-2xl">
             <Image
-              src="https://i.postimg.cc/9MgjTS2B/vogal.png"
+              src="https://i.postimg.cc/T3qFFnJ3/Grocer-Ease.png"
               alt="vogal"
               width={250}
               height={250}
@@ -98,61 +145,43 @@ const Projects = () => {
             </div>
           </div>
           <div className="border-b md:border-none py-12 md:py-0 pb-5 md:pb-0">
-            <p className="hidden md:block text-3xl">E-commerce</p>
-            <p className="hidden md:block mt-2">Mern stack</p>
+            <p className="hidden md:block text-3xl">GrocerEase</p>
+            <p className="hidden md:block mt-2">TS, Next.js</p>
             <p className="hidden md:block mt-5">Source code : Github</p>
-            <div className="flex gap-5 items-center">
-              <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
-                Frontend code
-                <MdArrowOutward />
-              </button>
-              <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
-                Backend code
-                <MdArrowOutward />
-              </button>
-              <div className="block md:hidden text-2xl">
-                <MdArrowOutward />
-              </div>
-            </div>
-            <div className="text-4xl md:mt-5 hidden md:flex justify-end">
-              <MdArrowOutward />
-            </div>
-          </div>
-        </div>
 
-        {/* project 04 */}
-        <div className="md:flex items-center gap-7 mt-16 md:mt-0">
-          <div className="flex items-center gap-2 md:gap-5 shadow-black shadow-2xl">
-            <Image
-              src="https://i.postimg.cc/9MgjTS2B/vogal.png"
-              alt="vogal"
-              width={250}
-              height={250}
-            ></Image>
-            <div className="block md:hidden">
-              <p className="text-xl">E-commerce</p>
-              <p className="mt-2">Mern stack</p>
-            </div>
-          </div>
-          <div className="border-b md:border-none py-12 md:py-0 pb-5 md:pb-0">
-            <p className="hidden md:block text-3xl">E-commerce</p>
-            <p className="hidden md:block mt-2">Mern stack</p>
-            <p className="hidden md:block mt-5">Source code : Github</p>
-            <div className="flex gap-5 items-center">
-              <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
-                Frontend code
-                <MdArrowOutward />
-              </button>
-              <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
-                Backend code
-                <MdArrowOutward />
-              </button>
+            <div className="flex gap-5 items-center mt-3">
+              <Link
+                href="https://github.com/FuadTalukder85/GrocerEase-frontend"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
+                  Frontend code
+                  <MdArrowOutward />
+                </button>
+              </Link>
+              <Link
+                href="https://github.com/FuadTalukder85/grocerEase-backend"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="text-white flex items-center gap-3 border-b border-[#ee2fee] hover:text-[#ee2fee] transition-all duration-700 px-3 py-2 rounded-lg">
+                  Backend code
+                  <MdArrowOutward />
+                </button>
+              </Link>
               <div className="block md:hidden text-2xl">
                 <MdArrowOutward />
               </div>
             </div>
             <div className="text-4xl md:mt-5 hidden md:flex justify-end">
-              <MdArrowOutward />
+              <Link
+                href="https://grocer-ease-chi.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <MdArrowOutward />
+              </Link>
             </div>
           </div>
         </div>
